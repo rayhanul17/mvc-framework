08ba57fcd312f7012829fa6b31caf7d16e9e5f1f
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2025-08-07 16:21:04 +0600

    remove old

--- Mvc.Framework/Areas/Book/Models/Dto/CategoryDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace mvc.framework.Areas.Book.Models.Dto
-{
-    public class CategoryDto
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string? Description { get; set; }
-    }
-}
