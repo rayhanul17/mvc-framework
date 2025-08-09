9b5ca8b7513839ca31b593a7dab70f2b32b2b1ba
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2025-08-09 14:44:30 +0600

    profile update page

---
+++ DynamicRoleMenuSystem.Application/Interfaces/IFileUploadService.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+
+namespace DynamicRoleMenuSystem.Application.Interfaces;
+
+public interface IFileUploadService
+{
+    Task<string?> UploadImageAsync(IFormFile file, string folderName = "uploads");
+    Task<bool> DeleteImageAsync(string filePath);
+    bool IsValidImage(IFormFile file);
+    string GetImageUrl(string? filePath);
+}
