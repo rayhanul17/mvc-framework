25d6e85ae3079266b596565a195393aba66f9448
Md. Rayhanul Islam Raj <rayhanulraj210@gmail.com>
2025-09-02 19:22:10 +0600

    audit log issue

---
+++ src/ModularHost.Web/Controllers/ProjectController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MRCMS.Controllers
+{
+    [Authorize]
+    public class ProjectController : Controller
+    {
+
+        public IActionResult Overview()
+        {
+            ViewData["Title"] = "Project Overview";
+            return View();
+        }
+
+        public IActionResult Documentation()
+        {
+            ViewData["Title"] = "Documentation";
+            return View();
+        }
+
+        public IActionResult API()
+        {
+            ViewData["Title"] = "API Documentation";
+            return View();
+        }
+    }
+}
